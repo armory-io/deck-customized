4f04ce79e633d77229e3ed0db1e0a4feab590010
Kevin Woo <kevinawoo@gmail.com>
2017-10-20 17:41:42 -0700

    add openstack as a module

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { module } from 'angular';
 import { CORE_MODULE } from '@spinnaker/core';
 import { AMAZON_MODULE } from '@spinnaker/amazon';
 import { GOOGLE_MODULE } from '@spinnaker/google';
+import { OPENSTACK_MODULE } from '@spinnaker/openstack';
 import { ACME_MODULE } from './acme/acme.module';
 
 module('acme.spinnaker', [
   CORE_MODULE,
   AMAZON_MODULE,
   GOOGLE_MODULE,
+  OPENSTACK_MODULE,
   ACME_MODULE,
 ]);
